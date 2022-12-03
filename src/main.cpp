97bf57e94f8265afc1908d6b2ba25bbf3f1c9634
Corey Fitzpatrick <cfitzpatrick616@gmail.com>
2022-12-03 02:22:01 -0500

    made data file names shorter

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,17 @@
 
 int main() {
 	std::cout << "--- Day 1: Calorie Counting ---" << std::endl;
-	DayOne d1("./src/data/day-one.txt");
+	DayOne d1("./src/data/day-1.txt");
 	d1.partOneSolution();
 	d1.partTwoSolution();
 
 	std::cout << "--- Day 2: Rock Paper Scissors ---" << std::endl;
-	DayTwo d2("./src/data/day-two.txt");
+	DayTwo d2("./src/data/day-2.txt");
 	d2.partOneSolution();
 	d2.partTwoSolution();
 
 	std::cout << "--- Day 3: Rucksack Reorganization ---" << std::endl;
-	DayThree d3("./src/data/day-three.txt");
+	DayThree d3("./src/data/day-3.txt");
 	d3.partOneSolution();
 	d3.partTwoSolution();
 }
