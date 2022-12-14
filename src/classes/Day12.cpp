487c417ae8a51ba8009b5540c80c04cdb887ef7e
Corey Fitzpatrick <cfitzpatrick616@gmail.com>
2022-12-13 22:07:23 -0500

    finished day 12

--- src/classes/Day12.cpp
+++ src/classes/Day12.cpp
@@ -1,11 +1,18 @@
 #include "Day12.h"
+#include <algorithm>
 
 Day12::Day12(std::string filename) : _filename(filename) {
 }
-int dist[150][150] = { 0 };
 std::tuple<int, int> end;
 
 void Day12::partOneSolution() {
+	std::vector<std::vector<char>> map;
+	std::deque<std::tuple<int, int>> q;
+	std::set<std::tuple<int, int>> visited;
+	std::vector<std::tuple<int, int>> startingPositions;
+	std::tuple<int, int> start;
+	std::tuple<int, int> end;
+
 	inFile.open(_filename);
 	while (std::getline(inFile, line)) {
 		std::vector<char> row;
@@ -14,8 +21,8 @@ void Day12::partOneSolution() {
 		}
 		map.push_back(row);
 	}
-	std::tuple<int, int> start;
-	std::tuple<int, int> end;
+	const int ROWS = map.size();
+	const int COLS = map[0].size();
 
 	for (int i = 0; i < map.size(); i++) {
 		for (int j = 0; j < map[i].size(); j++) {
@@ -30,14 +37,12 @@ void Day12::partOneSolution() {
 		}
 	}
 
-	const int ROWS = map.size();
-	const int COLS = map[0].size();
-	std::deque<std::tuple<int, int>> q;
-	std::set<std::tuple<int, int>> visited;
 	q.push_back(start);
 	visited.insert(start);
 	int distance;
-
+	std::vector<std::vector<int> > dist(
+		500,
+		std::vector<int>(500, 0));
 	while (!q.empty()) {
 		std::tuple<int, int> cur = q.front();
 		q.pop_front();
@@ -46,17 +51,74 @@ void Day12::partOneSolution() {
 		if (cur == end) {
 			distance = dist[x][y];  break;
 		}
-		addChildren(map, cur, std::make_tuple(x - 1, y), ROWS, COLS, q, visited);
-		addChildren(map, cur, std::make_tuple(x, y + 1), ROWS, COLS, q, visited);
-		addChildren(map, cur, std::make_tuple(x + 1, y), ROWS, COLS, q, visited);
-		addChildren(map, cur, std::make_tuple(x, y - 1), ROWS, COLS, q, visited);
+		addChildren(map, cur, std::make_tuple(x - 1, y), ROWS, COLS, q, visited, dist);
+		addChildren(map, cur, std::make_tuple(x, y + 1), ROWS, COLS, q, visited, dist);
+		addChildren(map, cur, std::make_tuple(x + 1, y), ROWS, COLS, q, visited, dist);
+		addChildren(map, cur, std::make_tuple(x, y - 1), ROWS, COLS, q, visited, dist);
 	}
 
 	std::cout << "Day 11-1: " << distance << std::endl;
+	inFile.close();
 }
 
 void Day12::partTwoSolution() {
-	std::cout << "Day 11-2: " << std::endl;
+	std::vector<std::vector<char>> map;
+	std::vector<std::tuple<int, int>> startingPositions;
+	std::tuple<int, int> start;
+	std::tuple<int, int> end;
+	int shortest = INT_MAX;
+
+	inFile.open(_filename);
+	while (std::getline(inFile, line)) {
+		std::vector<char> row;
+		for (char c : line) {
+			row.push_back(c);
+		}
+		map.push_back(row);
+	}
+	const int ROWS = map.size();
+	const int COLS = map[0].size();
+
+	for (int i = 0; i < map.size(); i++) {
+		for (int j = 0; j < map[i].size(); j++) {
+			if (map[i][j] == 'S' || map[i][j] == 'a') {
+				startingPositions.push_back(std::make_tuple(i, j));
+				map[i][j] = 'a';
+			}
+			if (map[i][j] == 'E') {
+				end = std::make_tuple(i, j);
+				map[i][j] = 'z';
+			}
+		}
+	}
+
+	for (std::tuple<int, int> startPos : startingPositions) {
+		std::deque<std::tuple<int, int>> q;
+		std::set<std::tuple<int, int>> visited;
+		q.push_back(startPos);
+		visited.insert(startPos);
+		int shortestPath = INT_MAX;
+		std::vector<std::vector<int> > dist(
+			ROWS,
+			std::vector<int>(COLS, 0));
+
+		while (!q.empty()) {
+			std::tuple<int, int> cur = q.front();
+			q.pop_front();
+			auto [x, y] = cur;
+
+			if (cur == end) {
+				shortest = dist[x][y] < shortest ? dist[x][y] : shortest;
+				break;
+			}
+
+			addChildren(map, cur, std::make_tuple(x - 1, y), ROWS, COLS, q, visited, dist);
+			addChildren(map, cur, std::make_tuple(x, y + 1), ROWS, COLS, q, visited, dist);
+			addChildren(map, cur, std::make_tuple(x + 1, y), ROWS, COLS, q, visited, dist);
+			addChildren(map, cur, std::make_tuple(x, y - 1), ROWS, COLS, q, visited, dist);
+		}
+	}
+	std::cout << "Day 11-2: " << shortest << std::endl;
 }
 
 void Day12::addChildren(std::vector<std::vector<char>>& map,
@@ -65,16 +127,15 @@ void Day12::addChildren(std::vector<std::vector<char>>& map,
 	int ROWS,
 	int COLS,
 	std::deque <std::tuple<int, int>>& q,
-	std::set<std::tuple<int, int>>& visited) {
+	std::set<std::tuple<int, int>>& visited,
+	std::vector<std::vector<int>>& dist
+) {
 	auto [i, j] = cur;
 	auto [x, y] = nextNode;
-
-	//std::cout << "current: " << i << "," << j << " x:" << x << " y : " << y << std::endl;
 
 	if (x < 0 || x == ROWS || y < 0 || y == COLS || visited.count(nextNode) || map[x][y] > map[i][j] + 1) {
 		return;
 	}
-
 	dist[x][y] = dist[i][j] + 1;
 	visited.insert(nextNode);
 	q.push_back(nextNode);
--- src/headers/Day12.h
+++ src/headers/Day12.h
@@ -15,12 +15,12 @@ private:
 	std::string _filename;
 	std::string line;
 	std::ifstream inFile;
-	std::vector<std::vector<char>> map;
 	void addChildren(std::vector<std::vector<char>>& map,
 		std::tuple<int, int> cur,
 		std::tuple<int, int> nextNode,
 		int ROWS,
 		int COLS,
 		std::deque <std::tuple<int, int>>& q,
-		std::set<std::tuple<int, int>>& visited);
+		std::set<std::tuple<int, int>>& visited,
+		std::vector<std::vector<int>>&);
 };
